a3b0a58b42eb55661eacc3a441ddd09aeb6f13c9
bmulhern2 <bmulhern09@gmail.com>
2020-04-02 09:54:19 -0400

    updated code

---
+++ week-7/personalPortfolio/main.js
@@ -0,0 +1,24 @@
+import { KeyObject } from "crypto"
+
+// This is my main.js file
+var projects = {}
+    {
+        id: 0,
+        name: "bioSite",
+        link: "https://bmulhern2.github.io/MichaelMulhernBiography/",
+        image: "images/bioSite.png"
+    },
+    {
+        id: 1,
+        name: "web-330",
+        link: "https://github.com/bmulhern2/web-330",
+        image: "images/bioSite.png"
+    },
+    {
+        id: 2,
+        name: "web-231",
+        link: "https://github.com/bmulhern2/web-231",
+        image: "images/web-231.png"
+    }];
+ko
+ko.applyBindings(myBinding(projects));
